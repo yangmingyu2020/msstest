7cd53809947d5cfe6f794d5e3872ad910dd0f8ec
yangmingyu <yangmingyu2020@gmail.com>
2022-08-11 16:07:14 +0900

    project initial setup

---
+++ msstest/src/main/java/com/musinsa/msstest/MsstestApplication.java
@@ -0,0 +1,13 @@
+package com.musinsa.msstest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsstestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsstestApplication.class, args);
+	}
+
+}
---
+++ msstest/src/test/java/com/musinsa/msstest/MsstestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.musinsa.msstest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsstestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
